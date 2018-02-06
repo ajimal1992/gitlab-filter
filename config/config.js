de819b48127384cbd339e437ae653fb7001e79c2
Mohamed Ajimal <ajimal@hotmail.com>
2018-02-06 22:06:34 +0800

    socketio working, trying multiple request

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ exports.HOME_FN = "home.html";
 exports.ERROR_404_FN = "error_404.html"
 exports.ERROR_FAILURE_FN = "error_failure.html"
 
-exports.SITE_DIR = "site/";
-var VIEWS_DIR = "views/";
-exports.HOME_VIEW_DIR = VIEWS_DIR + "home/";
-exports.ERROR_VIEW_DIR = VIEWS_DIR + "error/";
-exports.JS_DIR = "js/";
-exports.CSS_DIR = "css/";
-exports.SOCKET_DIR = "socket.io/"
+exports.SITE_DIR = "site";
+var VIEWS_DIR = "views";
+exports.HOME_VIEW_DIR = VIEWS_DIR + "home";
+exports.ERROR_VIEW_DIR = VIEWS_DIR + "error";
+exports.JS_DIR = "js";
+exports.CSS_DIR = "css";
+exports.SOCKET_DIR = "socket.io"
 
 //Port
 exports.PORT = 55555;
--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var request = require('request');
 
 //setup http express server
 var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var server = require('http').createServer(app);  //** create server before initiating the socket...
+
 
 //IO
 var io = require('socket.io')(server);
@@ -21,26 +23,86 @@ app.use('/' + config.JS_DIR,express.static(path.join(__dirname + '/', config.SIT
 app.use('/' + config.CSS_DIR,express.static(path.join(__dirname + '/', config.SITE_DIR, config.CSS_DIR)));
 app.use('/' + config.SOCKET_DIR + '/',express.static(path.join(__dirname + '/', config.SITE_DIR, config.SOCKET_DIR)));
 */
-app.use(express.static(__dirname));
-
-var server = require('http').createServer(app); 
-
-//Helmet Securing
-var helmet = require('helmet');
-app.use(helmet());
 
 //start server
 server.listen(config.PORT, function() {
     console.log('Server listening at port %d', config.PORT);
 });
 
+app.use(express.static(path.join(__dirname, config.SITE_DIR)));
+
 //IO
 io.on('connection', function(socket) {
+    /*
     io.emit("xxx_send_response", {msg:"sent"});
 
     socket.on("xxx_recv_request", function(data) {
         console.log(data);
         //process request
+    });
+    */
+
+    socket.on("search_repos", function(query){
+        var search_query = query['search'];
+        //console.log("Search: '" + search_query + "'");
+        var options_ori = {
+            url: config.GL_SERVER + 'api/v4/groups/' + config.GL_GRP_ID +'/projects?private_token=' + config.GL_TOKEN + '&simple=true&per_page=100&search='+search_query,
+            method: 'HEAD',
+            rejectUnauthorized: false,
+            requestCert: false,
+            agent: false
+        };
+        //console.log(options);
+        request.get(options,
+            function(error,response, body){
+                // console.log("Error: " + error); //TODO: If error send an error 400
+                console.log(response.headers);
+                var total_pages = parseInt(response.headers.x-total-pages);
+                var n = 1; //loop with delay
+                var request_delay = 200;
+                function request_page() {
+                    //request
+                    //var options = JSON.parse(JSON.stringify(options_ori));
+
+                    /*
+                    replace options with this...
+                    var x = "asdasdasdasd=asdsad&sasdda&page=13asasasassad";
+                    var y = x.substring(0,x.search("page=")+5) + 23;
+                    console.log(y);
+                    */
+                    n++;
+                    if( n < (total_pages+1)){
+                        setTimeout( request_page, request_delay);
+                    }
+                }
+                request_page();
+                var json_data = JSON.parse(body);
+                var data = [];
+                for(var i=0; i<json_data.length; i++){ //TODO: BETTER logic to remove duplicate forloop
+                    var id = json_data[i].id;
+                    var repo = json_data[i].name;
+                    var web_url = '<a href="' + json_data[i].web_url + '">' + json_data[i].web_url + '</a>';
+                    data.push([id,repo,web_url]);
+                    // console.log("id:" + id + ", repo:" + repo + ", web_url:"+ web_url);
+                }
+                //res.send(data);
+                io.emit("get_repos",data);
+
+                //clear memory
+                data=[]; 
+                delete body;
+                delete error;
+                var completed_page = 1;
+
+                //check if there are multiple pages
+                if("1" != response.headers.x-total-pages){
+                    var total_pages = parseInt(response.headers.x-total-pages);
+                    for(var i=2; i==total_pages; i++){
+
+                    }
+                }
+            }
+        );
     });
 });
 
@@ -49,7 +111,7 @@ io.on('connection', function(socket) {
 
 //home controller 
 app.get(config.HOME_ROUTE, function (req, res) {
-	renderView(res,config.HOME_VIEW_DIR,config.HOME_FN);
+	renderView(res,config.HOME_FN);
 });
 
 //repos controller
@@ -67,7 +129,7 @@ app.get(config.REPO_ROUTE, function(req, res){
 		function(error,response, body){
             var json_data = JSON.parse(body);
             // console.log("Error: " + error); //TODO: If error send an error 400
-            console.log(response.headers);
+            //console.log(response.headers);
             var data = [];
             for(var i=0; i<json_data.length; i++){
                 var id = json_data[i].id;
@@ -89,11 +151,11 @@ app.get("/", function(req, res){
     res.redirect(config.HOME_ROUTE);
 });
 
-//catch other routes (must be the end of all other routes)
-app.get("*", function(req, res){
-    res.status(404)
-    renderView(res,config.ERROR_VIEW_DIR,config.ERROR_404_FN);
-});
+// //catch other routes (must be the end of all other routes)
+// app.get("*", function(req, res){
+//     res.status(404)
+//     renderView(res,config.ERROR_404_FN);
+// });
 
 //You define error-handling middleware last, after other app.use() and routes calls
 app.use(function(err, req, res, next) {
@@ -102,10 +164,10 @@ app.use(function(err, req, res, next) {
     }
     console.error(err.stack)
     res.status(500)
-    renderView(res,config.ERROR_VIEW_DIR,config.ERROR_FAILURE_FN);
+    renderView(res,config.ERROR_FAILURE_FN);
 });
   
 //render view
-function renderView(res,VIEW_DIR,FILE){
-	res.sendFile(path.join(__dirname + '/', config.SITE_DIR, VIEW_DIR, FILE));
+function renderView(res,FILE){
+	res.sendFile(path.join(__dirname, config.SITE_DIR, FILE));
 }
